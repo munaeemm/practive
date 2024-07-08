40820b42536be4b87ce118667bc876b10bd361c3
munaeemm <114252605+munaeemm@users.noreply.github.com>
2024-07-08 17:32:05 +0100

    1

--- javascript/amazon.js
+++ javascript/amazon.js
@@ -25,8 +25,8 @@ let html = ` <div class="product">
 <img class="product-image" src="${item.image}" alt="Product 1">
 <h3 class="product-name">${item.name}</h3>
 <p class="price">$ ${(item.price/100).toFixed(2)}</p>
-<input type="number" value="1" min="1" max="99" class="product-quantity-box">
-<button class="button-add-to-cart" data-product-name = "${item.name}">Add to Cart</button>
+<input type="number" value="1" min="1" max="99" class="product-quantity-box" data-item-quantity = {this.value}>
+<button class="button-add-to-cart" data-item-name = "${item.name}">Add to Cart</button>
 </div> `;
 applyHTML += html;
 });
@@ -39,10 +39,26 @@ function displayProduct () {
 
 displayProduct();
 
-document.querySelectorAll('.button-add-to-cart').forEach((element) => {
-    element.addEventListener('click', () => {
-        c
+document.querySelectorAll('.button-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
 
+        const productName = button.dataset.itemName;
+        let duplicateItem;
+
+        cart.forEach((item) => {
+            if (productName === item.productName) {
+
+                duplicateItem = item;
+
+                console.log(`Duplicate item found: ${item.productName}`);
+            }
+        });
+
+        cart.push ({
+            inCartProductName: productName,
+            inCartQuantity:1
+        });
+console.log(cart);
     });
 });
 
